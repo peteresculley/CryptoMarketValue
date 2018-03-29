bdfdb5ee2c142f52288047c51aa80fab94e89cd7
petersculley <psculley3@gmail.com>
2018-03-29 17:08:58 +0700

    Use function instead of arrow functions for mocha
    Can't believe I forgot that this is needed

--- NodeJS/test/test.js
+++ NodeJS/test/test.js
@@ -8,29 +8,29 @@ const assert = require('assert');
 // Controllers
 const cryptoInfo = require('./../controllers/cryptoInfo');
 
-describe('cryptoInfo', () => {
-    describe('private:', () => {
-        describe('#cleanCurrency(currency)', () => {
-            it('should return lower case with spaces replaced by dashes', () => {
+describe('cryptoInfo', function() {
+    describe('private:', function() {
+        describe('#cleanCurrency(currency)', function() {
+            it('should return lower case with spaces replaced by dashes', function() {
                 assert.equal(cryptoInfo.__test_only__.cleanCurrency("BiTcOiN cAsH"), "bitcoin-cash");
             });
         });
-        describe('#getAPIUrl(cleanedCurrency)', () => {
-            it('should return COINMARKETCAP_API + cleanedCurrency', () => {
+        describe('#getAPIUrl(cleanedCurrency)', function() {
+            it('should return COINMARKETCAP_API + cleanedCurrency', function() {
                 assert.equal(cryptoInfo.__test_only__.getAPIUrl("bitcoin"), "https://api.coinmarketcap.com/v1/ticker/bitcoin");
             });
         });
-        describe('#createInfoObject(cryptoTickerElement)', () => {
-            it('should return value and marketValue of ticker element', () => {
+        describe('#createInfoObject(cryptoTickerElement)', function() {
+            it('should return value and marketValue of ticker element', function() {
                 let infoObject = cryptoInfo.__test_only__.createInfoObject({price_usd: 250, market_cap_usd: 5000});
                 assert.equal(infoObject.value, 250);
                 assert.equal(infoObject.marketValue, 5000);
             });
         });
     });
-    describe('public:', () => {
-        describe('#getInfo(currency, cb)', () => {
-            it('should return value and marketValue for currency', (done) => {
+    describe('public:', function() {
+        describe('#getInfo(currency, cb)', function() {
+            it('should return value and marketValue for currency', function(done) {
                 cryptoInfo.getInfo("Bitcoin", (error, info) => {
                     if(error) {
                         done(error);
@@ -42,7 +42,7 @@ describe('cryptoInfo', () => {
                     }
                 });
             });
-            it('should return error for non-existant currency', (done) => {
+            it('should return error for non-existant currency', function(done) {
                 cryptoInfo.getInfo("abcd", (error, info) => {
                     if(error) {
                         done();
